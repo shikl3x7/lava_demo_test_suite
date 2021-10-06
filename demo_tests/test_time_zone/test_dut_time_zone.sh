ed3194ff26364da35e5f3f71c5c1826973589358
Shivaschandra K L <shivaschandra_kl@mentor.com>
2021-10-06 14:48:41 +0530

    Added changes to compare DUT time with server time

--- demo_tests/test_time_zone/test_dut_time_zone.sh
+++ demo_tests/test_time_zone/test_dut_time_zone.sh
@@ -41,9 +41,24 @@ test_time_zone()
 	echo $output
 	if $(echo "$output" | grep -q "$test_seq") ;
 	then
-		report_pass "test_time_zone"
+		report_pass "Time zone set on DUT is UTC:"
 	else
-		report_fail "test_time_zone"
+		report_fail "Time zone set on DUT is not UTC:"
 	fi
+	actual_server_string=$(sntp  time.windows.com)
+    info_msg "Read back from time.windows.com : $actual_server_string"
+    server_time_string=$(sntp  time.windows.com | grep -o "[0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}")
+    info_msg "Server time captured from time.windows.com  :   $server_time_string"
+
+    actual_system_string=$(date)
+    info_msg "Read back system time : $actual_system_string"
+    system_time_string=$(date | grep -o "[0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}")
+    info_msg "System Time :  $system_time_string"
+    if $(echo "$system_time_string" | grep -q "$server_time_string") ;
+    then
+        report_pass "Compare DUT time and time.windows.com :"
+    else
+        report_fail "Compare DUT time and time.windows.com :"
+    fi
 }
 test_time_zone
